4b1900be5aecb010307a0481edc1ced88747cc76
BigFly2333 <jmf920692483@163.com>
2017-11-22 11:44:03 +0800

    jquery模块重复引入依赖bug fix

--- kealselect/jquery.kealselect.js
+++ kealselect/jquery.kealselect.js
@@ -1,9 +1,16 @@
 (function(root, factory){
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['jquery'], function(jquery) {
-      root.KealSelect = factory(jquery);
-    });
+    var $ = root.jQuery;
+    if (!$ || typeof $ !== 'function') {
+      define(['jquery'], function(jquery) {
+        root.KealSelect = factory(jquery);
+      });
+    } else {
+      define([], function() {
+        root.KealSelect = factory($);
+      });
+    }
   } else if (typeof module === 'object' && module.exports) {
       // Node. Does not work with strict CommonJS, but
       // only CommonJS-like environments that support module.exports,
